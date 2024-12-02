c410f7d9d9d0337768016abb5788b7629c894198
Milan den Besten <milan.den.besten@gmail.com>
2024-12-02 20:43:44 +0100

    Add to_string to Std.hs

--- compiler/Std.hs
+++ compiler/Std.hs
@@ -5,10 +5,11 @@ module Std where
 
 import Ast
     ( Type(Base, Var, Appl, Func),
-      Value(VIntrin, VString, VList, VNat, VInt, VChar, VBool, VSole) )
+      Value(VIntrin, VString, VList, VNat, VInt, VChar, VBool, VSole),
+      extractChar)
 import Types (Env (..))
 import qualified Data.HashMap.Strict as M
-import Data.Text (Text, unpack)
+import Data.Text (pack, Text, unpack)
 import Text.Read (readMaybe)
 import Data.Char ( ord, chr )
 import Control.Monad.IO.Class (liftIO, MonadIO)
@@ -24,6 +25,7 @@ env = Env { gamma = M.fromList [
         ("cons", (True, Func [(Nothing, Var "T" 1), (Nothing, Appl (Base "List") [Var "T" 1])] (Appl (Base "List") [Var "T" 1]))),
         ("sole", (True, Base "Trivial")),
         ("to_char_list", (True, Func [(Nothing, Base "String")] (Appl (Base "List") [Base "Char"]))),
+        ("to_string", (True, Func [(Nothing, (Appl (Base "List") [Base "Char"]))] (Base "String"))),
         ("to_ascii", (True, Func [(Nothing, Base "Char")] (Base "Nat"))),
         ("from_ascii", (True, Func [(Nothing, Base "Nat")] (Base "Char")))
     ], delta = M.empty, currentFunction = "" }
@@ -39,6 +41,7 @@ prelude = M.fromList [
     ,   ("False", VBool False)
     ,   ("sole", VSole)
     ,   ("to_char_list", VIntrin intrin_char_list)
+    ,   ("to_string", VIntrin intrin_to_string)
     ,   ("to_ascii", VIntrin intrin_to_ascii)
     ,   ("from_ascii", VIntrin intrin_from_ascii)
     ]
@@ -56,6 +59,8 @@ prelude = M.fromList [
             VList as -> return . VList $ a : as
         intrin_char_list [l] = case l of
             VString s -> return $ VList $ map VChar $ unpack s
+        intrin_to_string [s] = case s of
+            VList s -> return $ VString $ pack $ map extractChar s
         intrin_to_ascii [c] = case c of VChar c -> return $ VNat $ ord c
         intrin_from_ascii [n] = case n of
             VNat n -> return $ VChar $ chr n
